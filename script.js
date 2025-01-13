5b9447566ef486861e3149029fc679fa78d29079
emiratasayar <atasayaremir@gmail.com>
2025-01-13 15:43:02 +0300

    yüklendi

---
+++ script.js
@@ -0,0 +1,4 @@
+// Test verileri (Raspberry Pi’den gelen verileri simüle ediyoruz)
+document.getElementById('speed').innerText = '2 kmh';
+document.getElementById('direction').innerText = 'Kuzeybatı';
+document.getElementById('signal').innerText = '-30 dB';
